2a55c87338312d1dea142d43f3a545d0d489523a
TangChuYe <1711871238@qq.com>
2023-04-26 14:02:34 +0800

    封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,14 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
-export function login(data) {}
+// 登录接口封装
+export function login(data) {
+  // 返回一个axios对象 => promise  // 返回了一个promise对象
+  return request({
+    url: '/sys/login',
+    method: 'POST',
+    data
+  })
+}
 
 export function getInfo(token) {}
 

769ec0957f88f00119416d6d485254a0564c8c6c
TangChuYe <1711871238@qq.com>
2023-04-27 15:03:19 +0800

    实现权限拦截处调用获取资料action

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,7 +2,8 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   token: state => state.user.token, // 在根级的getters上 开发子模块的属性给别人看 给别人用
-  // avatar: state => state.user.avatar,
-  name: state => state.user.userInfo.username // 建立用户名称的映射
+  name: state => state.user.userInfo.username, // 建立用户名称的映射
+  userId: state => state.user.userInfo.userId, // 建立用户id的映射
+  staffPhoto: state => state.user.userInfo.staffPhoto // 建立用户头像的映射
 }
 export default getters
